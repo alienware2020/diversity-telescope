4cd6a4c253b8df11b8c6327cdaae505996aa94aa
Rajesh Kumar Kundu <rajeshkkundu@visionplannercloud.nl>
2021-04-26 23:40:27 +0530

    update to salary optimization

--- src/app/reward-recognition/reward-recognition.component.ts
+++ src/app/reward-recognition/reward-recognition.component.ts
@@ -12,34 +12,44 @@ export class RewardRecognitionComponent implements OnInit {
   showResponse: boolean = false;
   faCalculator = faCalculator;
   faPaperPlane = faPaperPlane;
-  ratio: number;
-  technicalQ1: number;
-  technicalQ2: number;
-  technicalQ3: number;
-  technicalQ4: number;
-  leadershipQ1: number;
-  leadershipQ2: number;
-  leadershipQ3: number;
-  leadershipQ4: number;
-  businessQ1: number;
-  businessQ2: number;
-  businessQ3: number;
-  businessQ4: number;
+  totalScore: string;
+  avgRating: string;
+  initiativeScore: number;
+  isEligibleForPromotion: boolean = false;
+  technicalQ1: number = 0;
+  technicalQ2: number = 0;
+  technicalQ3: number = 0;
+  technicalQ4: number = 0;
+  leadershipQ1: number = 0;
+  leadershipQ2: number = 0;
+  leadershipQ3: number = 0;
+  leadershipQ4: number = 0;
+  businessQ1: number = 0;
+  businessQ2: number = 0;
+  businessQ3: number = 0;
+  businessQ4: number = 0;
   form = new FormGroup({
     quarter: new FormControl('', Validators.required)
   });
-  
+
   constructor() { }
 
   ngOnInit() {
   }
 
-  onUpload() {
+  calculate() {
     this.showLoader = true;
     setTimeout(() => {
       this.showResponse = true;
       this.showLoader = false;
-      this.ratio = 4;
+      this.initiativeScore = 4.3;
+      let avgTechnicalRating = this.technicalQ1 + this.technicalQ2 + this.technicalQ3 + this.technicalQ4;
+      let avgLeadershipRating = this.leadershipQ1 + this.leadershipQ2 + this.leadershipQ3 + this.leadershipQ4;
+      let avgBusinessRating = this.businessQ1 + this.businessQ2 + this.businessQ3 + this.businessQ4;
+      let avgRating = ((avgTechnicalRating + avgLeadershipRating + avgBusinessRating) / 12);
+      this.avgRating = avgRating.toFixed(1);
+      this.totalScore = (avgRating * 0.8 + this.initiativeScore * 0.2).toFixed(1);
+      this.isEligibleForPromotion = (avgRating * 0.8 + this.initiativeScore * 0.2) > 2.5;
     }, 1000);
   }
 }
--- src/app/salary-optimization/salary-optimization.component.ts
+++ src/app/salary-optimization/salary-optimization.component.ts
@@ -15,8 +15,49 @@ export class SalaryOptimizationComponent implements OnInit {
   faCheck = faCheck;
   faCalculator = faCalculator;
   questions: any[];
+  grid: number = 1;
+  range1: number;
+  range2: number;
+  marginLeft: number;
+  markerMarginLeft: number;
   form = new FormGroup({
-    quarter: new FormControl('', Validators.required)
+    expectedSalary: new FormControl('', Validators.required),
+    panel11: new FormControl('', Validators.required),
+    panel12: new FormControl('', Validators.required),
+    panel13: new FormControl('', Validators.required),
+    panel14: new FormControl('', Validators.required),
+    panel15: new FormControl('', Validators.required),
+    panel16: new FormControl('', Validators.required),
+    panel17: new FormControl('', Validators.required),
+    panel18: new FormControl('', Validators.required),
+    panel19: new FormControl('', Validators.required),
+    panel110: new FormControl('', Validators.required),
+    panel111: new FormControl('', Validators.required),
+    panel112: new FormControl('', Validators.required),
+    panel113: new FormControl('', Validators.required),
+    panel114: new FormControl('', Validators.required),
+    panel115: new FormControl('', Validators.required),
+    panel116: new FormControl('', Validators.required),
+    panel117: new FormControl('', Validators.required),
+    panel118: new FormControl('', Validators.required),
+    panel21: new FormControl('', Validators.required),
+    panel22: new FormControl('', Validators.required),
+    panel23: new FormControl('', Validators.required),
+    panel24: new FormControl('', Validators.required),
+    panel25: new FormControl('', Validators.required),
+    panel26: new FormControl('', Validators.required),
+    panel27: new FormControl('', Validators.required),
+    panel28: new FormControl('', Validators.required),
+    panel29: new FormControl('', Validators.required),
+    panel210: new FormControl('', Validators.required),
+    panel211: new FormControl('', Validators.required),
+    panel212: new FormControl('', Validators.required),
+    panel213: new FormControl('', Validators.required),
+    panel214: new FormControl('', Validators.required),
+    panel215: new FormControl('', Validators.required),
+    panel216: new FormControl('', Validators.required),
+    panel217: new FormControl('', Validators.required),
+    panel218: new FormControl('', Validators.required),
   });
 
   constructor() {
@@ -45,11 +86,96 @@ export class SalaryOptimizationComponent implements OnInit {
   ngOnInit() {
   }
 
-  onUpload() {
+  onSubmit() {
     this.showLoader = true;
     setTimeout(() => {
       this.showResponse = true;
       this.showLoader = false;
+      this.calculate();
+      this.calculateMarkerPosition()
     }, 1000);
   }
+
+  calculateMarkerPosition() {
+    let expectedSalary = this.form.get('expectedSalary').value;
+    if (expectedSalary && !isNaN(expectedSalary)) {
+      let salary = parseFloat(expectedSalary);
+      let perc = (salary - 30) / 6;
+      this.markerMarginLeft = (360 * perc) - 5;
+    }
+  }
+
+  calculate() {
+    let panel1Value: number = 0;
+    let panel1Count: number = 0;
+    for (let i = 1; i <= 18; i++) {
+      let panel1 = this.form.get('panel1' + i).value;
+      if (panel1 && !isNaN(panel1)) {
+        panel1Count++;
+        panel1Value += parseInt(panel1);
+      }
+    }
+
+    let panel2Value: number = 0;
+    let panel2Count: number = 0;
+    for (let i = 1; i <= 18; i++) {
+      let panel2 = this.form.get('panel2' + i).value;
+      if (panel2 && !isNaN(panel2)) {
+        panel2Count++;
+        panel2Value += parseInt(panel2);
+      }
+    }
+    let totalScore = (panel1Value + panel2Value);
+    let average = totalScore / (panel1Count + panel2Count);
+
+    if (average >= 0 && average <= 1) {
+      this.grid = 1;
+      this.range1 = 30;
+      this.range2 = 32;
+      this.marginLeft = 0;
+    }
+    else if (average > 1 && average <= 2) {
+      this.grid = 2;
+      this.range1 = 32;
+      this.range2 = 34;
+      this.marginLeft = 120;
+    }
+    else if (average > 2 && average <= 3) {
+      this.grid = 3;
+      this.range1 = 34;
+      this.range2 = 36;
+      this.marginLeft = 240;
+    }
+    else {
+      this.grid = 0;
+      this.range1 = 0;
+      this.range2 = 0;
+      this.marginLeft = 0;
+    }
+
+    // if (totalScore >= 18 && totalScore < 30) {
+    //   this.grid = 1;
+    //   this.range1 = 30;
+    //   this.range2 = 32;
+    // }
+    // else if (totalScore >= 30 && totalScore < 42) {
+    //   this.grid = 2;
+    //   this.range1 = 32;
+    //   this.range2 = 34;
+    // }
+    // else if (totalScore >= 42 && totalScore <= 54) {
+    //   this.grid = 3;
+    //   this.range1 = 34;
+    //   this.range2 = 36;
+    // }
+    // else {
+    //   this.grid = 0;
+    //   this.range1 = 0;
+    //   this.range2 = 0;
+    // }
+  }
+
+  onApprove() { }
+
+  onCancel() { }
 }
